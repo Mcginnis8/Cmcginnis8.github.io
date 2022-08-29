b18637bb120f23eca449f5f0532af54d12668abb
Cole McGinnis <cmcginnis8@gatech.edu>
2022-08-29 17:48:27 -0400

    UI updates and Contact Page

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ window.onload = function() {
   counter = 0;
   cover = document.getElementById('cover');
   cover.addEventListener('click', opener);
+  $('.pop-up').hide(0);
+  $('#Node3').click(function(){
+    $('#cover').hide(0);
+    $('#Node0').hide(0);
+    $('#Node1').hide(0);
+    $('#Node2').hide(0);
+    $('#Node3').hide(0);
+    $('.pop-up').fadeIn(500);
+  });
+  $('.pop-up span').click(function() {
+    $('.pop-up').hide(500);
+    $('#cover').show(0);
+    $('#Node0').show(0);
+    $('#Node1').show(0);
+    $('#Node2').show(0);
+    $('#Node3').show(0);
+  });
 }
 
 function opener() {
